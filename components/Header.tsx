a717dd077d234b2656f92f1632eef3a63db75b8a
R3v180 <123840995+R3v180@users.noreply.github.com>
2023-11-26 08:17:47 +0100

    Commit from Stub AI (#1)
    Co-authored-by: R3v180 <bot@codestub.ai>

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-blue-500 text-white p-5">
+      <h1 className="text-xl">Encryption & Decryption App</h1>
+    </header>
+  );
+};
+
+export default Header;
